80e849279cfea2ab5216daab11c61ebea22a2c07
Antonio Neto <antonio.asn03@gmail.com>
2019-09-18 21:08:49 -0300

    Modificação da classe Dados

--- Lista de Exercícios 01/src/Dados.py
+++ Lista de Exercícios 01/src/Dados.py
@@ -1,29 +1,35 @@
+import random
 class Dados:
 
     def __init__(self):
-        self.dados = []
-        self.classes = []
+        self.treino = []
+        self.teste = []
 
 
-    def importarDados(self, nomeDoArquivo, nomeClasse):
+    def importarDados(self, aqv1, aqv2, aqv3, nome1, nome2, nome3):
+        dados = self.pegarAqv(aqv1, nome1) + self.pegarAqv(aqv2, nome2) + self.pegarAqv(aqv3, nome3)
+        random.shuffle(dados)
+
+        for k in range(270):
+            self.treino.append(dados[k])                    #COLOCA 60% DOS DADOS PARA TREINO
+
+        for k in range(270, len(dados)):
+            self.teste.append(dados[k])                     #COLOCA 40# DOS DADOS PARA TESTE
+
+
+
+    def pegarAqv(self, nomeDoArquivo, nomeClasse):
 
         arquivo = open(nomeDoArquivo, 'r')
         frases = arquivo.readlines()                        #LENDO TODAS AS FRASES DO ARQUIVO
+        classe = []
 
         for k in range(len(frases)):                        #REMOVENDO OS \N's DAS STR
-            frases[k] = frases[k].rstrip('\n')
-
-            self.dados.append(frases[k])                    #COLOCANDO AS STR NA LIST DE DADOS
-            self.classes.append(nomeClasse)                 #COLOCANDO AS CLASSES NA LIST DE CLASSES
+            frases[k] = frases[k].rstrip('\n')              #COLOCANDO AS STR NA LIST DE DADOS
+            classe.append(nomeClasse)                       #COLOCANDO AS CLASSES NA LIST DE CLASSES
 
         arquivo.close()
 
-#######################################################################################################################
-                                        #TESTES#
-#######################################################################################################################
+        dados = list(zip(frases, classe))                  #JUNTA AS LISTAS EM UMA
 
-# d = Dados()
-# d.importarDados('/home/antonio/Documentos/RP-2019.2/Lista de Exercícios 01/frases/funny.txt', 'teste')
-#
-# print(d.dados)
-# print(d.classes)
+        return dados
