225b994a2389440edc772190e30584dda7e7709a
Antonio Neto <antonio.asn03@gmail.com>
2019-10-05 16:37:57 -0300

    Implementacao finalizada da lista

--- Lista de Exercícios 01/src/ArvoreDeDecisao.py
+++ Lista de Exercícios 01/src/ArvoreDeDecisao.py
@@ -10,7 +10,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
 
-
 # Instanciando arquivos para listas
 script_dir = os.path.dirname(__file__)                  # Diretório absoluto
 rel_path1 = "../frases/peace.txt"                       # Diretório relativo
@@ -20,10 +19,10 @@ abs_file_path1 = os.path.join(script_dir, rel_path1)    # Diretório final
 abs_file_path2 = os.path.join(script_dir, rel_path2)    # Diretório final
 abs_file_path3 = os.path.join(script_dir, rel_path3)    # Diretório final
 
-script = os.path.dirname(__file__)                  # Diretório absoluto
-path1 = "../frasesMais/PeaceMais.txt"                       # Diretório relativo
-path2 = "../frasesMais/SilenceMais.txt"                     # Diretório relativo
-path3 = "../frasesMais/SuccessMais.txt"                     # Diretório relativo
+script = os.path.dirname(__file__)          # Diretório absoluto
+path1 = "../frasesMais/PeaceMais.txt"       # Diretório relativo
+path2 = "../frasesMais/SilenceMais.txt"     # Diretório relativo
+path3 = "../frasesMais/SuccessMais.txt"     # Diretório relativo
 file_path1 = os.path.join(script, path1)    # Diretório final
 file_path2 = os.path.join(script, path2)    # Diretório final
 file_path3 = os.path.join(script, path3)    # Diretório final
@@ -61,25 +60,25 @@ bagOfWords = y_vect.fit_transform(testeFrase).toarray()  # coloca todas as frase
 
 arvore = tree.DecisionTreeClassifier()  # cria arvore
 arvore.fit(treinoFrase, treinoClass)  # treina arvore
+
 # mostra score
 print("A precisão é: ", str(arvore.score(bagOfWords, testeClass) * 100) + "%")
+
 # classifica a frase
 frase = [str(input("Digite a frase: "))]    #recebe a frase e coloca em um vetor
 fraseMatriz = x.transform(frase).toarray()  #transforma a frase em vertor
-print("A classificação é: ",str(arvore.predict(fraseMatriz)))   #infere a classificacao
+print("A classificação é: ",str(arvore.predict(fraseMatriz)[0]))   #infere a classificacao
+print("O caminho é: \n", str(arvore.decision_path(fraseMatriz)))
 
-#print("O caminho é: ", str(arvore.decision_path(fraseMatriz)))
-
+#gerar imagem da arvore
 dot_data = tree.export_graphviz(arvore)
 graph = graphviz.Source(dot_data)
 graph.render("arvoreImagem")
 
-
 #validacao cruzada
-
 #pegar dados
 dadosMais = dados.importarDadosMaiores(file_path1, file_path2, file_path1, "Peace", "Silence", "Success")
 #validacao
 cruzada = cross_val_score(arvore, dadosMais[0], dadosMais[1], cv=5) #calcular as validacoes
 cruzada = (cruzada[0] + cruzada[1] + cruzada[2] + cruzada[3] + cruzada[4])/5 #calcular medias
-print("A média de precisão por validação cruzada é: ", str(cruzada * 100) + "%")
+print("\nA média de precisão por validação cruzada é: ", str(cruzada * 100) + "%")
